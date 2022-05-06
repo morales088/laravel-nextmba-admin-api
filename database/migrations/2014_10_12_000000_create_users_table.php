49edb8d90886f661525ab494c275de75eff08ecc
Neil <johnneilmorales@gmail.com>
2022-05-06 23:38:46 +0800

    admin login using passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            // $table->rememberToken();
+            $table->integer()->comment('[0 - deleted, 1 - active]');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::prefix("/user")->group( function (){
+    Route::post("/login", "api\loginController@login");
+    Route::middleware("auth:api")->get("/all", "api\loginController@index");
 });
