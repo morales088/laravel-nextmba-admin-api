63b84dc5ddeed0d0bae64227e950eebca5008818
Neil <johnneilmorales@gmail.com>
2022-05-09 12:41:33 +0800

    changing login to personal access token

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix("/user")->group( function (){
-    Route::post("/login", "api\loginController@login");
+    Route::post("/login", "api\loginController@personalAccessLogin");
     Route::middleware("auth:api")->get("/all", "api\loginController@index");
 });
