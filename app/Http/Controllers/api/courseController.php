c60808dbd1073a1942fe326735c123a8d2766533
neil <johnneilmorales8@gmail.com>
2022-05-29 23:32:57 +0800

    broadcast status bug fix

--- app/Http/Controllers/api/courseController.php
+++ app/Http/Controllers/api/courseController.php
@@ -90,7 +90,7 @@ class courseController extends Controller
                     ],
             'broadcast_status' => [
                         'string',
-                        Rule::in(['upcoming', 'live', 'pending_replay', 'replay']),
+                        Rule::in(['offline', 'live', 'pending_replay', 'replay']),
                     ],
         ]);
         
@@ -102,7 +102,7 @@ class courseController extends Controller
             $request['status'] = 3;
         }
 
-        if($request->broadcast_status == "upcoming"){
+        if($request->broadcast_status == "offline"){
             $request['broadcast_status'] = 1;
         }elseif($request->broadcast_status == "live"){
             $request['broadcast_status'] = 2;
@@ -126,7 +126,7 @@ class courseController extends Controller
                         
         $getmodule = COLLECT(\DB::SELECT("select m.*, 
                                         (CASE WHEN m.status = 1 THEN 'draft' WHEN m.status = 2 THEN 'published' WHEN m.status = 3 THEN 'archived' END) module_status,
-                                        (CASE WHEN m.broadcast_status = 1 THEN 'upcoming' WHEN m.broadcast_status = 2 THEN 'live' WHEN m.broadcast_status = 3 THEN 'pending_replay' WHEN m.broadcast_status = 4 THEN 'replay' END) broadcast_status,
+                                        (CASE WHEN m.broadcast_status = 1 THEN 'offline' WHEN m.broadcast_status = 2 THEN 'live' WHEN m.broadcast_status = 3 THEN 'pending_replay' WHEN m.broadcast_status = 4 THEN 'replay' END) broadcast_status,
                                         t.name topic_name
                                         from modules m 
                                         left join topics t ON t.id = m.topicId
@@ -152,7 +152,7 @@ class courseController extends Controller
         //                                 from speakers where moduleId = $request->id and status <> 0");
         $module = COLLECT(\DB::SELECT("select m.*, 
                                         (CASE WHEN m.status = 1 THEN 'draft' WHEN m.status = 2 THEN 'published' WHEN m.status = 3 THEN 'archived' END) module_status,
-                                        (CASE WHEN m.broadcast_status = 1 THEN 'upcoming' WHEN m.broadcast_status = 2 THEN 'live' WHEN m.broadcast_status = 3 THEN 'pending_replay' WHEN m.broadcast_status = 4 THEN 'replay' END) broadcast_status,
+                                        (CASE WHEN m.broadcast_status = 1 THEN 'offline' WHEN m.broadcast_status = 2 THEN 'live' WHEN m.broadcast_status = 3 THEN 'pending_replay' WHEN m.broadcast_status = 4 THEN 'replay' END) broadcast_status,
                                         t.name topic_name
                                         from modules m 
                                         left join topics t ON t.id = m.topicId
@@ -190,7 +190,7 @@ class courseController extends Controller
 
         $modules = DB::SELECT("select m.*, 
                                 (CASE WHEN m.status = 1 THEN 'draft' WHEN m.status = 2 THEN 'published' WHEN m.status = 3 THEN 'archived' END) module_status,
-                                (CASE WHEN m.broadcast_status = 1 THEN 'upcoming' WHEN m.broadcast_status = 2 THEN 'live' WHEN m.broadcast_status = 3 THEN 'pending_replay' WHEN m.broadcast_status = 4 THEN 'replay' END) broadcast_status,
+                                (CASE WHEN m.broadcast_status = 1 THEN 'offline' WHEN m.broadcast_status = 2 THEN 'live' WHEN m.broadcast_status = 3 THEN 'pending_replay' WHEN m.broadcast_status = 4 THEN 'replay' END) broadcast_status,
                                 t.name topic_name
                                 from modules m 
                                 left join topics t ON t.id = m.topicId
