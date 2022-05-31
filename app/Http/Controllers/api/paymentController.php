5a852d06006588d20f2ef87bfffde1aa7774ea4c
neil <johnneilmorales8@gmail.com>
2022-05-31 16:37:17 +0800

    change branch

--- app/Http/Controllers/api/paymentController.php
+++ app/Http/Controllers/api/paymentController.php
@@ -15,41 +15,41 @@ class paymentController extends Controller
 {
     public function createPayment(Request $request){
         $payment = $request->validate([
-            'reference_id' => 'string',
-            'email' => 'string',
-            'phone' => 'string',
-            'first_name' => 'string',
-            'last_name' => 'string',
-            'country' => 'string',
-            'product' => 'string',
-            'url' => 'string',
-            'utm_source' => 'string',
-            'utm_medium' => 'string',
-            'utm_campaign' => 'string',
-            'utm_content' => 'string',
+            'reference_id' => 'required|string',
+            'email' => 'required|string',
+            'phone' => 'required|string',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'country' => 'required|string',
+            'product' => 'required|string',
+            'url' => 'required|string',
+            'utm_source' => 'required|string',
+            'utm_medium' => 'required|string',
+            'utm_campaign' => 'required|string',
+            'utm_content' => 'required|string',
         ]);
 
         // CHECK IF ACCOUNT ALREADY EXISTING, IF NOT CREATE ACCOUNT
-        $checker = DB::SELECT("SELECT * FROM students where email = " . $request->email);
+        // $checker = DB::SELECT("SELECT * FROM students where email = " . $request->email);
 
         // CREATE PAYMENT
-        $payment = Payment::create(
-        [
-            'reference_id ' => $request->reference_id,
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'country' => $request->country,
-            'product' => $request->product,
-            'amount' => $request->amount,
-            'status' => "Unpaid",
-            'url' => $request->url,
-            'utm_source' => $request->utm_source,
-            'utm_medium' => $request->utm_medium,
-            'utm_campaign' => $request->utm_campaign,
-            'utm_content' => $request->utm_content
-        ]);
+        // $payment = Payment::create(
+        // [
+        //     'reference_id ' => $request->reference_id,
+        //     'first_name' => $request->first_name,
+        //     'last_name' => $request->last_name,
+        //     'email' => $request->email,
+        //     'phone' => $request->phone,
+        //     'country' => $request->country,
+        //     'product' => $request->product,
+        //     'amount' => $request->amount,
+        //     'status' => "Unpaid",
+        //     'url' => $request->url,
+        //     'utm_source' => $request->utm_source,
+        //     'utm_medium' => $request->utm_medium,
+        //     'utm_campaign' => $request->utm_campaign,
+        //     'utm_content' => $request->utm_content
+        // ]);
 
         $paymentItems = [];
         if(str_contains($request->product, "Bundle")) {
@@ -72,61 +72,69 @@ class paymentController extends Controller
             $item = [2, $qty];
             array_push($paymentItems);
         }
+        
+
+        dd($paymentItems, str_contains($request->product, "Marketing"));
     }
 
 
     public function completePayment(Request $request){
+
         $payment = $request->validate([
             'reference_id' => 'required|string',
         ]);
 
+        // // create user/student 
+        // $checker = DB::SELECT("SELECT * FROM students where email = '$request->email'");
+
+        // $student = null;
+        // $password = Payment::generate_password();
+        // if(!empty($checker)){
+        //     // CREATE NEW ACCOUNT
+        //     $student = DB::transaction(function() use ($request) {
+
+        //         $student = Student::create($request->only('email', 'name', 'country', 'phone', 'country') + 
+        //             [
+        //                 'name' => $payment->first_name . ' ' . $payment->last_name,
+        //                 'email' => $payment->email,
+        //                 'password' => Hash::make($password),
+        //                 'updated_at' => now()
+        //             ]);
+
+        //         return $student;
+        //     });
+        // } 
+
         $payment = Payment::find($request->reference_id);
         // DD($payment);
 
-
         // UPDATE PAYMENT
         $payment = DB::SELECT("SELECT * FROM payments where reference_id = " . $request->reference_id);
-        $payment = DB::UPDATE("SELECT * FROM payments where reference_id = " . $request->reference_id);
 
         $payment->update(
             [ 
+                // 'student_id' => ,
                 'ID' => $payment->id,
                 'status' => "Paid",
             ]
         );
 
-        $student = null;
-        $password = generate_password();
-        if(!empty($checker)){
-            // CREATE NEW ACCOUNT
-            $student = DB::transaction(function() use ($request) {
-
-                $student = Student::create($request->only('email', 'name', 'country', 'phone', 'country') + 
-                    [
-                        'name' => $payment->first_name . ' ' . $payment->last_name,
-                        'email' => $payment->email,
-                        'password' => Hash::make($password),
-                        'updated_at' => now()
-                    ]);
-
-                return $student;
-            });
-        } 
+        
 
       
     }
 
-    function generate_password($length = 20){
-        $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    // function generate_password($length = 20){
+    //     $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
       
-        $str = '';
-        $max = strlen($chars) - 1;
+    //     $str = '';
+    //     $max = strlen($chars) - 1;
       
-        for ($i=0; $i < $length; $i++)
-          $str .= $chars[random_int(0, $max)];
+    //     for ($i=0; $i < $length; $i++)
+    //       $str .= $chars[random_int(0, $max)];
       
-        return $str;
-      }
+    //     return $str;
+    //   }
 
 
     public function sendEmailAndPassword(Request $request, $email, $password)
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -11,4 +11,16 @@ class Payment extends Model
     
     protected $guarded = ['id'];
     protected $table = 'payments';
+
+    public static function generate_password($length = 20){
+        $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      
+        $str = '';
+        $max = strlen($chars) - 1;
+      
+        for ($i=0; $i < $length; $i++)
+          $str .= $chars[random_int(0, $max)];
+      
+        return $str;
+    }
 }
