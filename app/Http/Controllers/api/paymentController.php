a0fc2ae122c0a84dc75b73be35224ee20d00d4f1
neil <johnneilmorales8@gmail.com>
2022-06-11 01:52:53 +0800

    bug fix

--- app/Http/Controllers/api/paymentController.php
+++ app/Http/Controllers/api/paymentController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Mail;
 use App\Mail\AccountCredentialEmail;
 use App\Models\User;
