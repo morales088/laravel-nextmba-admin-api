5d42b3d2f30dc63ffa035e4e36fd455909dbfd10
neil <johnneilmorales8@gmail.com>
2022-06-20 22:38:56 +0800

    change branch

--- app/Console/Commands/AccountSeed.php
+++ app/Console/Commands/AccountSeed.php
@@ -35,7 +35,7 @@ class AccountSeed extends Command
     {
         // return 0;
         $path = base_path().'/public/csv/account.csv';
-
+        // dd(true);
         if(file_exists($path)){
             $file = new \SplfileObject($path);
             $file->setFlags(\SplfileObject::READ_CSV);
@@ -43,65 +43,69 @@ class AccountSeed extends Command
             $account = DB::transaction(function() use ($file) {
                 
                 foreach ($file as $key => $value) {
-                                      
-                    list($email, $name, $status, $date_created, $last_login, $courses) = $value;
                     
-                    $date_created = date('Y-m-d H:i:s', strtotime($date_created));
-                    $expiration_date = date('Y-m-d H:i:s', strtotime('+1 year', strtotime($date_created)));
-                    // dd($email, $name, $status, $date_created, $last_login, $courses, explode(",", $courses));
+                    // check if value i null
 
-                    // check duplicate on db
-                    $check = DB::SELECT("SELECT * FROM students where email = '$email'");
-
-                    // dd($check, $status, empty($check), $status == "Active", empty($check) && $status == "Active");
-                    
-                    // check if active
-                    if(empty($check) && $status == "Active"){
-                        
-                        // create student
-                        $password = Payment::generate_password();
-                        
-                        $student = Student::create(
-                        [
-                            'name' => $name,
-                            'email' => $email,
-                            'password' => $password,
-                            'created_at' => $date_created,
-                        ]);
-                        
-
-                        // send account credentials to email
-                        $user = [
-                            'email' => $email,
-                            'password' => $password
-                        ];
-                        Mail::to($email)->send(new AccountCredentialEmail($user));
+                    if(!empty($value[0])){
+                        list($email, $name, $status, $date_created, $last_login, $courses) = $value;
 
                         
-                        // if $courses has "Course" word str_contains($courses, "Course")
-                        if(str_contains($courses, "Course")){
-                            $student_courses = explode(",", $courses);
+                        $date_created = date('Y-m-d H:i:s', strtotime($date_created));
+                        $expiration_date = date('Y-m-d H:i:s', strtotime('+1 year', strtotime($date_created)));
+                        // dd($email, $name, $status, $date_created, $last_login, $courses, explode(",", $courses));
+                        dd($date_created, $expiration_date);
+                        // check duplicate on db
+                        $check = DB::SELECT("SELECT * FROM students where email = '$email'");
 
-                            // dd($student_courses, $date_created, $expiration_date);
+                        // dd($check, $status, empty($check), $status == "Active", empty($check) && $status == "Active");
+                        
+                        // dd(empty($check) && $status == "Active", empty($check), $status == "Active");
+                        // check if active
+                        if(empty($check) && $status == "Active"){
+                            
+                            // create student
+                            $password = Payment::generate_password();
+                            
+                            $student = Student::create(
+                            [
+                                'name' => $name,
+                                'email' => $email,
+                                'password' => $password,
+                                'created_at' => $date_created,
+                            ]);
+                            
 
-                            foreach ($student_courses as $key1 => $value1) {
+                            // send account credentials to email
+                            // $user = [
+                            //     'email' => $email,
+                            //     'password' => $password
+                            // ];
+                            // Mail::to($email)->send(new AccountCredentialEmail($user));
+
+                            // if $courses has "Course" word str_contains($courses, "Course")
+                            if(str_contains($courses, "Course")){
+                                $student_courses = explode(",", $courses);
                                 
-                                if(str_contains($value1, "Marketing")){
-                                    //insert marketing course to student
-                                    $info = ['studentId' => $student->id, 'courseId' => 1, 'qty' => 1, 'starting_date' => $date_created, 'expiration_date' => $expiration_date];
-                                    Studentcourse::insertStudentCourse($info);
-                                }
+                                // dd($student_courses, $date_created, $expiration_date);
 
-                                if(str_contains($value1, "Executive")){
-                                    //insert Executive course to student
-                                    $info = ['studentId' => $student->id, 'courseId' => 2, 'qty' => 1, 'starting_date' => $date_created, 'expiration_date' => $expiration_date];
-                                    Studentcourse::insertStudentCourse($info);
+                                foreach ($student_courses as $key1 => $value1) {
+                                    
+                                    if(str_contains($value1, "Marketing")){
+                                        //insert marketing course to student
+                                        $info = ['studentId' => $student->id, 'courseId' => 1, 'qty' => 1, 'starting_date' => $date_created, 'expiration_date' => $expiration_date];
+                                        Studentcourse::insertStudentCourse($info);
+                                    }
+
+                                    if(str_contains($value1, "Executive")){
+                                        //insert Executive course to student
+                                        $info = ['studentId' => $student->id, 'courseId' => 2, 'qty' => 1, 'starting_date' => $date_created, 'expiration_date' => $expiration_date];
+                                        Studentcourse::insertStudentCourse($info);
+                                    }
                                 }
                             }
+                                
                         }
-                            
                     }
-
                 }
             });
         }
