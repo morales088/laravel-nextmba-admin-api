01ddde94a7db635f51eb06951c505552965c6614
Neil <johnneilmorales@gmail.com>
2022-12-02 21:42:36 +0800

    payment bug fix

--- app/Http/Controllers/api/paymentController.php
+++ app/Http/Controllers/api/paymentController.php
@@ -361,6 +361,7 @@ class paymentController extends Controller
             $validation['country'] = 'string';
             $validation['country'] = 'string';
 
+            $request->query->add(['name' => $request->full_name]);
         }
         
         $payment = $request->validate($validation);
@@ -374,9 +375,9 @@ class paymentController extends Controller
             $status = $request->paid == "true" ? "Paid" : "Unpaid" ; 
             
             // CREATE PAYMENT
-            $payment = Payment::create($request->only('reference_id', 'hitpay_id', 'phone', 'payment_method', 'product', 'country', 'url', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_content') +
+            $payment = Payment::create($request->only('name', 'reference_id', 'hitpay_id', 'phone', 'payment_method', 'product', 'country', 'url', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_content') +
             [
-                'name' => $request->full_name,
+                // 'name' => $request->full_name,
                 'email' => $request->email,
                 'price' => $request->price,
                 'status' => $status
