7f79f2ce532c129d9c693e92db04503051ca6514
Neil <johnneilmorales@gmail.com>
2023-01-18 14:15:13 +0800

    upload library files

--- app/Models/Libraryfile.php
+++ app/Models/Libraryfile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Libraryfile extends Model
 {
@@ -11,4 +12,14 @@ class Libraryfile extends Model
     
     protected $guarded = ['id'];
     protected $table = 'library_files';
+
+    public static function uploadFiles($request){
+
+        $filePath = 'images/library/'.time().'.'.$request->file->extension();
+
+        $path = Storage::disk('s3')->put($filePath, fopen($request->file('file'), 'r+'));
+        $path = Storage::disk('s3')->url($filePath);
+        
+        return $path;
+    }
 }
