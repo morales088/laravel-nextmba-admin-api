844802bcc7e25ba2db4ca018c7e427a3402f0a24
Neil <johnneilmorales@gmail.com>
2023-04-24 21:00:15 +0800

    tk-165

--- app/Console/Commands/SplitCourse.php
+++ app/Console/Commands/SplitCourse.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\Models\Student;
 use App\Models\Studentcourse;
 use App\Models\Payment;
+use App\Models\Courseinvitation;
 use DB;
 
 class SplitCourse extends Command
@@ -33,15 +34,14 @@ class SplitCourse extends Command
     {
         $DBtransaction = DB::transaction(function() {
 
-            $students = DB::SELECT("SELECT * FROM studentcourses where courseId = 3");
+            // transfer student access from id 3 to id 1&2
+            $student_courses = DB::SELECT("SELECT * FROM studentcourses where courseId = 3");
 
-            foreach ($students as $key => $value) {
+            foreach ($student_courses as $key => $value) {
 
                 $check_course_1 = DB::SELECT("SELECT * FROM studentcourses where studentId = $value->studentId and courseId = 1 and status = 1");
                 $check_course_2 = DB::SELECT("SELECT * FROM studentcourses where studentId = $value->studentId and courseId = 2 and status = 1");
                 
-                // dd($value);
-
                 if(empty($check_course_1)){
 
                     $studentCourse = new Studentcourse;
@@ -65,13 +65,44 @@ class SplitCourse extends Command
     
                     $studentCourse->save();
                 }
-                
-                // dd($value, empty($check_course_1), empty($check_course_2));
 
                 $this->line("Student Id: $value->studentId");
             }
             
-            // dd($students);
+
+            $student_gifts = DB::SELECT("SELECT * FROM course_invitations WHERE course_id = 3");
+
+            foreach ($student_gifts as $key => $value) {
+                $check_course_1 = DB::SELECT("SELECT * FROM course_invitations ci WHERE email = '$value->email' and course_id = 1");
+                $check_course_2 = DB::SELECT("SELECT * FROM course_invitations ci WHERE email = '$value->email' and course_id = 2");
+
+                    if(empty($check_course_1)){
+
+                        $studentGift = new Courseinvitation;
+                        $studentGift->from_student_id = $value->from_student_id;
+                        $studentGift->from_payment_id = $value->from_payment_id;
+                        $studentGift->course_id = 1;
+                        $studentGift->email = $value->email;
+                        $studentGift->status = 2;
+        
+                        $studentGift->save();
+                    }
+
+                    if(empty($check_course_2)){
+
+                        $studentGift = new Courseinvitation;
+                        $studentGift->from_student_id = $value->from_student_id;
+                        $studentGift->from_payment_id = $value->from_payment_id;
+                        $studentGift->course_id = 2;
+                        $studentGift->email = $value->email;
+                        $studentGift->status = 2;
+        
+                        $studentGift->save();
+                    }
+
+                $this->line("Student emil: $value->email");
+            }
+
         });
 
         $this->line("DONE.");
