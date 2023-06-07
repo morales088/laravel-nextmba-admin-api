9f5bc936dd7d9e695f726444a0f11452e02935aa
Neil <johnneilmorales@gmail.com>
2023-06-08 00:57:34 +0800

    partner code

---
+++ database/migrations/2023_06_07_124059_add_partner_code_to_payments.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {        
+        if (!Schema::hasColumn('payments', 'partner_code'))
+        {
+            Schema::table('payments', function (Blueprint $table) {
+                $table->string('partner_code')->after('url')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn('partner_code');
+        });
+    }
+};
