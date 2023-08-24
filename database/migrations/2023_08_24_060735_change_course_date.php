a40e09b55c0b4bdc19a387ccd0c1c1d6078726fd
Neil <johnneilmorales@gmail.com>
2023-08-24 14:21:13 +0800

    course date bug fix

---
+++ database/migrations/2023_08_24_060735_change_course_date.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn('students', 'course_date'))
+        {
+            Schema::table('students', function (Blueprint $table) {
+                $table->dateTime('course_date')->default('CURRENT_TIMESTAMP')->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
