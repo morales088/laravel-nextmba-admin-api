503666034ade7d12475313db2cf5e4006e91e543
Christ John Mercurio <christjohn.mercurio04@gmail.com>
2023-09-27 16:40:11 +0800

    add withoutOverlapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('students:assign-groups')->daily();
+        $schedule->command('students:assign-groups')->daily()->withoutOverlapping();
     }
 
     /**
