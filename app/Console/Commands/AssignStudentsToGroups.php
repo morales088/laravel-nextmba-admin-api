4f6c04e462482a4a0d725c77e8ec28c1c8e3e928
Christ John Mercurio <christjohn.mercurio04@gmail.com>
2023-10-02 18:10:20 +0800

    updated implementation

--- app/Console/Commands/AssignStudentsToGroups.php
+++ app/Console/Commands/AssignStudentsToGroups.php
@@ -45,63 +45,72 @@ class AssignStudentsToGroups extends Command
             $allowedRequests = $rateLimit / $perMinute;
             $chunkSize = 50; // Number of students to process in each chunk
 
-            Student::orderBy('created_at', 'desc')->take(900)->chunk($chunkSize, function ($students) use ($allowedRequests) {
-                $requestsMade = 0;
+            $twentyFourHoursAgo = now()->subHours(24);
 
-                foreach ($students as $student) {
-                    // Check if you've reached the allowed rate limit
-                    if ($requestsMade >= $allowedRequests) {
-                        sleep(5); // delay
-                        $requestsMade = 0; // Reset requests counter
-                    }
+            // Process students created or updated in the last 24 hours
+            Student::where(function ($query) use ($twentyFourHoursAgo) {
+                    $query->where('created_at', '>=', $twentyFourHoursAgo)
+                        ->orWhere('updated_at', '>=', $twentyFourHoursAgo);
+                })
+                ->chunk($chunkSize, function ($students) use ($allowedRequests) {
+                    $requestsMade = 0;
 
-                    if (!filter_var(mb_strtolower($student->email), FILTER_VALIDATE_EMAIL)) {
-                        Log::notice("Skipping student with invalid email: {$student->email}");
-                        continue;
-                    }
-
-                    if (empty($student->email)) {
-                        Log::notice("Skipping student with no email.");
-                        continue;
-                    }
-
-                    $normalizedEmail = mb_strtolower($student->email);
-
-                    // Retrieve unique course IDs associated with the student
-                    $uniqueCourseIds = $student->courses->pluck('courseId')->unique()->values()->toArray();
-
-                    // Upsert a new subscriber
-                    $subscriber = ['email' => $normalizedEmail];
-                    $this->mailerLite->subscribers->create($subscriber);
-
-                    $subscriber = $this->mailerLite->subscribers->find($subscriber['email']);
-                    $subscriberId = $subscriber['body']['data']['id'];
-
-                    // Check if student has a pro account
-                    if ($student->account_type === 3) {
-                        $this->mailerLite->groups->assignSubscriber(
-                            env('PRO_ACCOUNTS_GROUP_ID'), $subscriberId
-                        );
-                    }
-
-                    // Check if student is deactivated
-                    if ($student->status !== 0) {
-                        // Retrieve all subscriber group records based on course IDs
-                        $allGroupCourses = SubscriberGroup::whereIn('course_id', $uniqueCourseIds)->get();
-
-                        foreach ($allGroupCourses as $group) {
-                            // Assign the subscriber email to the group
-                            $this->mailerLite->groups->assignSubscriber($group->mailerlite_group_id, $subscriberId);
+                    foreach ($students as $student) {
+                        // Check if you've reached the allowed rate limit
+                        if ($requestsMade >= $allowedRequests) {
+                            sleep(5); // delay
+                            $requestsMade = 0; // Reset requests counter
                         }
-                    } else {
-                        $this->mailerLite->subscribers->delete($subscriberId);
-                    }
-                    // Update the requests counter
-                    $requestsMade++;
 
-                    Log::info("Student: {$student->email} processed successfully.");
-                }
-            });
+                        if (!filter_var(mb_strtolower($student->email), FILTER_VALIDATE_EMAIL)) {
+                            Log::notice("Skipping student with invalid email: {$student->email}");
+                            continue;
+                        }
+
+                        if (empty($student->email)) {
+                            Log::notice("Skipping student with no email.");
+                            continue;
+                        }
+
+                        $normalizedEmail = mb_strtolower($student->email);
+
+                        // Retrieve unique course IDs associated with the student
+                        $uniqueCourseIds = $student->courses->where('status', 1)
+                            ->pluck('courseId')->unique()->values()->toArray();
+
+                        // Upsert a new subscriber
+                        $subscriber = ['email' => $normalizedEmail];
+                        $this->mailerLite->subscribers->create($subscriber);
+
+                        $subscriber = $this->mailerLite->subscribers->find($subscriber['email']);
+                        $subscriberId = $subscriber['body']['data']['id'];
+
+                        // Check if student has a pro account
+                        if ($student->account_type === 3) {
+                            $this->mailerLite->groups->assignSubscriber(
+                                env('PRO_ACCOUNTS_GROUP_ID'), $subscriberId
+                            );
+                        }
+
+                        // Check if student is deactivated
+                        if ($student->status !== 0) {
+                            // Retrieve all subscriber group records based on course IDs
+                            $allGroupCourses = SubscriberGroup::whereIn('course_id', $uniqueCourseIds)->get();
+
+                            foreach ($allGroupCourses as $group) {
+                                // Assign the subscriber email to the group
+                                $this->mailerLite->groups->assignSubscriber($group->mailerlite_group_id, $subscriberId);
+                            }
+                        } else {
+                            $this->mailerLite->subscribers->delete($subscriberId);
+                            Log::info("Student: {$student->email} removed successfully.");
+                        }
+                        // Update the requests counter
+                        $requestsMade++;
+
+                        Log::info("Student: {$student->email} processed successfully.");
+                    }
+                });
 
             Log::info('Students have been assigned to subscriber groups.');
         
